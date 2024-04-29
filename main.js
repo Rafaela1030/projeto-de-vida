6efda742046b746f11c8821526f919aefe205428
Rafaela1030 <160487227+Rafaela1030@users.noreply.github.com>
2024-04-29 08:40:57 -0300

    ok

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
+const textos=document.querySelectorAll(".aba-conteudo");
 
 for(let i=0; i<botoes.length;i++){
     botoes[i].onclick=function(){
